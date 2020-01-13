4795d69fc6159eaa254295ee02169d2bad2e9fc7
Matthew Bloom <matthewabloom88@gmail.com>
2020-01-12 21:04:55 -0800

    update

--- game/entities/camera/scripts/camera.gd
+++ game/entities/camera/scripts/camera.gd
@@ -1,12 +1,26 @@
 extends Camera2D
 
-# Camera
+# Panning
 export (float) var pan_speed = 0.0
+
+# Zooming
+export (float) var zoom_factor = 0.0
 export (float) var zoom_speed = 0.0
+export (float) var zoom_margin = 0.0
+var _zoom_pos = Vector2()
+
+# Debug
+var _is_debugging = false
 
 ################################################################################
 # BUILT-IN VIRTUAL METHODS (CANNOT OVERRIDE)
 ################################################################################
+
+func _draw():
+	if _is_debugging:
+		draw_circle(position, 50, Color.red)
+
+#-------------------------------------------------------------------------------
 
 func _unhandled_input(event):
 	_check_zoom(event)
@@ -22,23 +36,18 @@ func _process(delta):
 ################################################################################
 
 func _check_inputs(delta):
-	var display_size = Utils.get_display_size()
-	var right_stop = limit_right - (display_size.x * zoom.x) 
-	var bottom_stop = limit_bottom  - (display_size.y * zoom.y) 
+	var x_input = int(Input.is_action_pressed('pan_right')) - \
+		int(Input.is_action_pressed('pan_left'))
+	var y_input = int(Input.is_action_pressed('pan_down')) - \
+		int(Input.is_action_pressed('pan_up'))
 	
-	if Input.is_action_pressed('pan_up'):
-		position.y = clamp(position.y - pan_speed * delta, limit_top,
-			bottom_stop)
-	elif Input.is_action_pressed('pan_down'):
-		position.y = clamp(position.y + pan_speed * delta, limit_top,
-			bottom_stop)
-		
-	if Input.is_action_pressed('pan_left'):
-		position.x = clamp(position.x - pan_speed * delta, limit_left,
-			right_stop)
-	elif Input.is_action_pressed('pan_right'):
-		position.x = clamp(position.x + pan_speed * delta, limit_left,
-			right_stop) 
+	var new_position = position + (Vector2(x_input, y_input).normalized() * \
+		pan_speed * delta)
+	
+	position.x = clamp(lerp(position.x, new_position.x, pan_speed * delta),
+		limit_left, limit_right)
+	position.y = clamp(lerp(position.y, new_position.y, pan_speed * delta),
+		limit_top, limit_bottom)
 
 #-------------------------------------------------------------------------------
 
@@ -48,7 +57,7 @@ func _check_zoom(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_WHEEL_UP:
-				zoom.x = clamp(zoom.x - zoom_speed, 1.0, 2.0)
+				zoom.x = clamp(zoom.x - zoom_factor, 1.0, 2.0)
 				zoom.y = clamp(zoom.y - zoom_speed, 1.0, 2.0)
 				position = mouse_pos
 			elif event.button_index == BUTTON_WHEEL_DOWN:
@@ -59,6 +68,18 @@ func _check_zoom(event):
 ################################################################################
 # PUBLIC METHODS
 ################################################################################
+
+func activate_debug_mode():
+	_is_debugging = true
+	update()
+
+#-------------------------------------------------------------------------------
+
+func deactivate_debug_mode():
+	_is_debugging = false
+	update()
+
+#-------------------------------------------------------------------------------
 
 func set_limits(limits):
 	"""
